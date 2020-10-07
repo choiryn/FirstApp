8b7e7bc1e9b8013c3b7a6b70bc9040a74a8b4214
choiryn <mamiedoreen@naver.com>
2020-10-07 23:07:22 +0900

    Commit Test

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 최린 on 2020/10/07.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //Commit Test
+        }
+    }
+}
